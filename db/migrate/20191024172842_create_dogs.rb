b32518c63d7d10a1e54dc38c3cc77c68dae3fef2
Annie Truex <artruex@gmail.com>
2019-10-24 17:34:31 +0000

    Automatically backed up by Learn

--- db/migrate/20191024172842_create_dogs.rb
+++ db/migrate/20191024172842_create_dogs.rb
@@ -1,12 +1,10 @@
 class CreateDogs < ActiveRecord::Migration[5.2]
-  def up #create table with name and breed 
-    create_table :dogs do |t|
-      t.string :name
-      t.string :breed
+
+  def change
+    create_table :dogs |t|
+      t.string :name 
+      t.string :breed 
     end
   end
   
-  def down #drop table
-    drop_table :dogs
-  end
 end
